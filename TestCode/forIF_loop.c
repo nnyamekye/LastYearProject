a06e9b873792825a357c9d5138f51ba49d8ce96f
nnyamekye <n.nyamekye@hotmail.co.uk>
2014-06-02 01:56:43 +0100

    Sample code
    sample code

---
+++ TestCode/forIF_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+    int i,count;
+    count = 0;
+
+    for ( i = 1 ; i <= 3 ; i++ ) {
+       if (i == 3){
+	count++;
+       }
+    }
+    
+    return 0;
+}
---
+++ TestCode/nestedforloop.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    int x,y,count;
+    
+    for ( x = 1; x <= 2 ; x++ ) {
+    	for ( y = 1 ; y <= 2 ; y++ ) {
+		count++;
+    	}
+    }
+    
+    return 0;
+}
